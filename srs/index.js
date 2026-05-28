6be4c6c3b491c743a0aa6ae1112b48d483eef255
tidye265 <moskonx@gmail.com>
2026-05-28 11:54:40 +0200

    Update index.js

--- srs/index.js
+++ srs/index.js
@@ -4,7 +4,6 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import './index.tsx';
 import './index.scss';
 import App from './app';
 import { Provider } from './context';
